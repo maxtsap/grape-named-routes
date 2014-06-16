4e71739a7745151ae76e453c7692d6480bb62714
Maxim Tsaplin <maxim.tsaplin@anahoret.com>
2014-06-16 16:27:14 +0300

    Initial commit.

---
+++ lib/grape/named/routes.rb
@@ -0,0 +1,9 @@
+require "grape/named/routes/version"
+
+module Grape
+  module Named
+    module Routes
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/grape/named/routes/version.rb
@@ -0,0 +1,7 @@
+module Grape
+  module Named
+    module Routes
+      VERSION = "0.0.1"
+    end
+  end
+end
